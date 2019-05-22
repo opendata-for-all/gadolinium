998602b7ede21aaad2d8ea795858287df37f2f9a
steven <steven.bucaille.etu@univ-lille.fr>
2019-05-23 00:21:54 +0200

    Added Slave testApi message
    Took 2 minutes

--- slave/index.js
+++ slave/index.js
@@ -1,29 +1,40 @@
 let io = require('socket.io-client');
 let region = process.argv[2];
-// let instanceName = process.argv[3];
-let instanceName = "api-1-asia-east2-b";
+let instanceName = process.argv[3];
+// let instanceName = "api-1-europe-north1-a";
 let masterConfig = require('./gadolinium-master');
 
-// let socket = io(`ws://${masterConfig.ipaddress}:${masterConfig.port}`, {
-// 	query : {
-// 		token : 'slave',
-// 		name : instanceName
-// 	}
-// });
-let socket = io(`ws://localhost:8080`, {
-	query: {
-		token: 'slave',
-		name: instanceName
+let socket = io(`ws://${masterConfig.ipaddress}:${masterConfig.port}`, {
+	query : {
+		token : 'slave',
+		name : instanceName
 	}
 });
+// let socket = io(`ws://localhost:8080`, {
+// 	query: {
+// 		token: 'slave',
+// 		name: instanceName
+// 	}
+// });
 
 socket.on('connect', () => {
 	console.log("Connected to Master");
-	setTimeout(() => {
-		socket.disconnect('reason');
-		setTimeout(() => {
-			socket.connect();
-		}, 5000);
-	}, 5000)
+	// setTimeout(() => {
+	// 	socket.disconnect('reason');
+	// 	setTimeout(() => {
+	// 		socket.connect();
+	// 	}, 5000);
+	// }, 5000)
 });
 
+socket.on('testApi', (data) => {
+	console.log(data);
+	let i = 1;
+	let apiId = data.apiId;
+	setInterval(() => {
+		socket.emit('testProgress', {apiId: apiId, slaveName: instanceName, progress: i + 1, totalProgress: 20});
+		i++;
+		if (i === 20) clearInterval(interval);
+	}, 5000);
+
+});
