e07ba704324858701d37ac197e78eb53e97f2dc0
steven <steven.bucaille.etu@univ-lille.fr>
2019-05-22 15:02:37 +0200

    Slave management for connection/disconnection of the master
    Took 3 hours 26 minutes

--- master/socketServerManagement/socketServerSlaveManagement.js
+++ master/socketServerManagement/socketServerSlaveManagement.js
@@ -1,23 +1,78 @@
 let APIStatusFunc = require('../APIStatus/functions');
 let socketServerFunc = require('./socketServerCreationAndConnection');
 
-let slavesBooting = {
-	testSlave: 1
+//Variables initialized in case master crashes and has to restart
+let slavesBooting = {};
+let slavesTesting = {};
+let slavesWaiting = {};
+let slavesToDelete = [];
+
+let addSlavesThatAreBootingOrTesting = () => {
+	let APIStatus = APIStatusFunc.getAPIStatus();
+	let apis = Object.keys(APIStatus);
+	apis.map((api) => {
+		let servers = Object.keys(APIStatus[api].servers);
+		servers.map((server) => {
+			if (APIStatus[api].servers[server].status === "Booting up") {
+				slavesBooting[server] = api
+			} else if (APIStatus[api].servers[server].status === "Testing") {
+				slavesTesting[server] = api
+			} else if (APIStatus[api].servers[server].status === "Waiting for tests") {
+				slavesWaiting[server] = api
+			}
+			APIStatus[api].servers[server].status = "Waiting for connection...";
+		})
+	});
+};
+
+let addSlaveToDeleteList = (slaveName) => {
+	slavesToDelete.push(slaveName);
 };
 
 let addSlaveToBootingSlaveList = (slaveName, apidId) => {
 	slavesBooting[slaveName] = apidId;
 };
 
-let serverBootedUp = (slaveClient, serverName) => {
+let slaveConnected = (slaveClient, slaveName) => {
 	let APIStatus = APIStatusFunc.getAPIStatus();
-	let apiId = slavesBooting[serverName];
-	APIStatus[apiId].servers[serverName].status = "Waiting for tests";
+	let apiId;
+	if ((apiId = slavesBooting[slaveName])) {
+		APIStatus[apiId].servers[slaveName].status = "Waiting for tests";
+	} else if ((apiId = slavesWaiting[slaveName])) {
+		APIStatus[apiId].servers[slaveName].status = "Waiting for tests";
+	} else if ((apiId = slavesTesting[slaveName])) {
+		APIStatus[apiId].servers[slaveName].status = "Testing";
+	}
 	APIStatusFunc.writeAPIStatus(APIStatus);
 	socketServerFunc.emitAPIStatusUpdate();
 };
 
+let slaveDisconnected = (slaveName, apiId) => {
+	let APIStatus = APIStatusFunc.getAPIStatus();
+	APIStatus[apiId].servers[slaveName].status = "Disconnected. Waiting for reconnection...";
+	APIStatusFunc.writeAPIStatus(APIStatus);
+	socketServerFunc.emitAPIStatusUpdate();
+};
+
+let handleDisconnection = (slaveClient, slaveName, slaveCallback) => {
+	slaveClient.on('disconnect', () => {
+		console.log(slaveName + " disconnected");
+		let reason;
+		let apiId = slavesTesting[slaveName] ? slavesTesting[slaveName] : slavesWaiting[slaveName];
+		if (apiId) { //TODO Difference from a server stopped running and a server stopped cause of tests finished
+			reason = "unwantedDisconnection";
+			slaveDisconnected(slaveName, apiId);
+		} else if (slavesToDelete.includes(slaveName)) {
+			reason = "vmDeleted";
+			slavesToDelete.remove(slaveName);
+		}
+		slaveCallback(reason);
+	})
+};
+
 module.exports = {
-	serverBootedUp: serverBootedUp,
-	addSlaveToBootingSlaveList: addSlaveToBootingSlaveList
+	slaveConnected: slaveConnected,
+	addSlaveToBootingSlaveList: addSlaveToBootingSlaveList,
+	addSlavesThatAreBootingOrTesting: addSlavesThatAreBootingOrTesting,
+	handleDisconnection: handleDisconnection,
 };
