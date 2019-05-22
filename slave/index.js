1c9386179bc534fc7963f6e20924420cbfa058cc
steven <steven.bucaille.etu@univ-lille.fr>
2019-05-22 15:03:01 +0200

    Slave behaviour at launch
    Took 23 seconds

--- slave/index.js
+++ slave/index.js
@@ -1,15 +1,29 @@
 let io = require('socket.io-client');
 let region = process.argv[2];
-let instanceName = process.argv[3];
+// let instanceName = process.argv[3];
+let instanceName = "api-1-asia-east2-b";
 let masterConfig = require('./gadolinium-master');
 
-let socket = io(`ws://${masterConfig.ipaddress}:${masterConfig.port}`, {
-	query : {
-		token : 'slave',
-		name : instanceName
+// let socket = io(`ws://${masterConfig.ipaddress}:${masterConfig.port}`, {
+// 	query : {
+// 		token : 'slave',
+// 		name : instanceName
+// 	}
+// });
+let socket = io(`ws://localhost:8080`, {
+	query: {
+		token: 'slave',
+		name: instanceName
 	}
 });
 
 socket.on('connect', () => {
 	console.log("Connected to Master");
+	setTimeout(() => {
+		socket.disconnect('reason');
+		setTimeout(() => {
+			socket.connect();
+		}, 5000);
+	}, 5000)
 });
+
