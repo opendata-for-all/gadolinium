be7d48572aa56942a16e2706667b07c134cadaa6
steven <steven.bucaille.etu@univ-lille.fr>
2019-06-05 19:21:30 +0200

    Set outpath build to back-end/master/expressManagement/public and reset default public path in express middleware

--- back-end/master/expressManagement/expressCreation.js
+++ back-end/master/expressManagement/expressCreation.js
@@ -20,7 +20,7 @@ let createApp = () => {
 	app.use(logger('dev'));
 	app.use(express.urlencoded({extended: false}));
 	app.use(cookieParser());
-	app.use(express.static(path.join(__dirname, 'public/dist/front-end')));
+	app.use(express.static(path.join(__dirname, 'public')));
 	app.set('views', path.join(__dirname, 'views'));
 	app.set('view engine', 'pug');
 	return app;
