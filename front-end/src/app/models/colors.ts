4ad4a885abfb2a5f3e08472cb6515cd71b1ea8b4
steven <steven.bucaille.etu@univ-lille.fr>
2019-06-16 17:21:39 +0200

    Added color for file validation

---
+++ front-end/src/app/models/colors.ts
@@ -0,0 +1,28 @@
+export const FileValidityColors = {
+  // IS VALID : https://material.io/tools/color/#!/?view.left=1&view.right=0&primary.color=43A047
+  isValid: {
+    text: {
+      primary: 'black',
+      light: 'black',
+      dark: 'white',
+    },
+    background: {
+      primary: '#43a047',
+      light: '#76d275',
+      dark: '#00701a'
+    }
+  },
+  // NOT VALID https://material.io/tools/color/#!/?view.left=1&view.right=1&primary.color=f43036
+  notValid: {
+    background: {
+      primary: '#f43036',
+      light: '#ff6b61',
+      dark: '#b9000e',
+    },
+    text: {
+      primary: 'black',
+      light: 'black',
+      dark: 'white'
+    }
+  }
+};
