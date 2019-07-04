5eb83922d49aa758fdd1b32290a7ae215eb2b3be
steven <steven.bucaille.etu@univ-lille.fr>
2019-07-04 18:26:56 +0200

    Bootscript changes

--- back-end/bootScript.sh
+++ back-end/bootScript.sh
@@ -7,7 +7,7 @@ git clone https://github.com/SOM-Research/gadolinium
 git clone https://github.com/baskaran-md/gcp-metadata
 INSTANCENAME=$(./gcp-metadata/gcp-metadata -n | cut -d ':' -f 2 | cut -d ' ' -f 2)
 REGION=$(./gcp-metadata/gcp-metadata -z | cut -d ':' -f 2 | cut -d ' ' -f 2)
-cd gadolinium
+cd gadolinium/back-end/
 npm install
 node index.js "slave" "$REGION" "$INSTANCENAME"
 
