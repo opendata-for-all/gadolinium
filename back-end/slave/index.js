d22843524cd3b9124b1c29d8a33facfea44d91b6
steven <steven.bucaille.etu@univ-lille.fr>
2019-08-06 12:16:05 +0200

    Slave connection fix

--- back-end/slave/index.js
+++ back-end/slave/index.js
@@ -5,7 +5,13 @@ let performanceTestFunc = require('./performanceTestFunc');
 let instanceName = process.argv[2];
 // let instanceName = require('../master/APIStatus/APIStatus').APIStatus[0].servers[0].name;
 let masterConfig = require('./gadolinium-master');
-// let socket = io(`ws://${masterConfig.ipaddress}:${masterConfig.port}`, {
+let socket = io(`ws://${masterConfig.ipaddress}:${masterConfig.port}`, {
+	query: {
+		token: 'slave',
+		name: instanceName
+	}
+});
+// let socket = io(`ws://localhost:8080`, {
 // 	query: {
 // 		token: 'slave',
 // 		name: instanceName
